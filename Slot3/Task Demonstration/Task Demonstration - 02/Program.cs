c05e6c1252e33aa4b7edbf4ebcad95a3fefb5e3b
hungnd29523 <anhdoi7496@gmail.com>
2025-01-15 21:10:41 +0700

    Slot3 + Lab1,2

---
+++ Slot3/Task Demonstration/Task Demonstration - 02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+class Program
+{
+    public static void Main()
+    {
+        Task[] tasks = new Task[5];
+        String taskData = "Hello";
+        for (int i = 0; i < 5; i++)
+        {
+            tasks[i] = Task.Run(() =>
+            {
+                Console.WriteLine($"Task ={Task.CurrentId},obj ={taskData}," +
+                    $"ThreadId={Thread.CurrentThread.ManagedThreadId}");
+                Thread.Sleep(1000);
+            });
+        }
+        try
+        {
+            Task.WaitAll(tasks);
+        }
+        catch (AggregateException ar)
+        {
+            Console.WriteLine("Oe pr more exception occured: ");
+            foreach(var ex in ar.Flatten().InnerExceptions)
+                Console.WriteLine("   {0}",ex.Message);
+        }
+        Console.WriteLine("Startus of completed task: ");
+        foreach (var t in tasks)
+        {
+            Console.WriteLine("  Task #{0}: {1}", t.Id, t.Status);
+        }
+            
+        }
+}
---
+++ Slot3/Task Demonstration/Task Demonstration - 04/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var tasks = new List<Task<int>>();
+        var source = new CancellationTokenSource();
+        var token = source.Token;
+        int completedIteretions = 0;
+        for (int n = 1; n <= 20; n++)
+        {
+            tasks.Add(Task.Run(() =>
+            {
+                int iterations = 0;
+                for (int ctr = 1; ctr <= 2_000_000; ctr++)
+                {
+                    token.ThrowIfCancellationRequested();
+                    iterations++;
+                }
+                Interlocked.Increment(ref completedIteretions);
+                if (completedIteretions >= 10)
+                    source.Cancel();
+                return iterations;
+            },token));
+        }
+        Console.WriteLine("Waitting for the first 10 task to complete...\n");
+        try
+        {
+            Task.WaitAll(tasks.ToArray());
+        }
+        catch (AggregateException)
+        {
+            Console.WriteLine("Status of task:\n");
+            Console.WriteLine("{0,10} {1,20} {2,14:N0}", "Task Id",
+                "Status", "Iterations");
+            foreach (var t in tasks)
+                Console.WriteLine("{0,10} {1,20} {2,14}",
+                    t.Id, t.Status,
+                    t.Status != TaskStatus.Canceled ?
+                    t.Result.ToString("N0") : "n/a");
+        }
+        Console.ReadLine();
+    }
+}
